c9792ab4482c7ee8e78643d05aedd5749a5206c4
Henrik Korsgaard <korsgaard@cs.au.dk>
2018-02-14 14:56:48 +0100

    just a small test

---
+++ ckan/ckan-analyser.js
@@ -0,0 +1,3 @@
+let analyse = require("../json-autotype/json-autotype.js")
+
+analyse.analyseJSON({simple:"test", character:"r", int: 10, bool:true, "simple2":"testing", "float": 10.3, "boolean": "true", anArray:[10, "sting", 10.3, false]})
---
+++ json-autotype/json-autotype.js
@@ -0,0 +1,19 @@
+exports.analyseJSON = function(json){
+
+    console.log(json.float)
+
+    return false
+}
+
+function recursiveAnalysis(data){
+    if(data.isArray()){
+
+    } else {
+        for( var key in data){
+            //we want to test the value
+        }
+    }
+    //test if array -> then we want to iterate
+
+    //test if key / value
+}
